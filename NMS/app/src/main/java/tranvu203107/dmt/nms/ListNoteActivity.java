637884e52ca540fc51cee5d6d4fee61dde4b8382
DucHuy0509 <17110305@student.hcmute.edu.vn>
2021-05-05 18:24:06 +0700

    Edit login and link get Id

--- NMS/app/src/main/java/tranvu203107/dmt/nms/ListNoteActivity.java
+++ NMS/app/src/main/java/tranvu203107/dmt/nms/ListNoteActivity.java
@@ -7,15 +7,27 @@ import androidx.drawerlayout.widget.DrawerLayout;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import android.content.Intent;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ListView;
+import android.widget.TextView;
 
 import com.google.android.material.navigation.NavigationView;
 
 import java.util.ArrayList;
 
 public class ListNoteActivity extends AppCompatActivity {
+    //Create database name and her path
+    String DATABASE_NAME="myDB.sqlite";
+    String DB_PATH_SUFFIX="/databases/";
+    SQLiteDatabase database = null;
+    int Id;
+    TextView txtNameUser;
+
     Toolbar toolbar;
     DrawerLayout drawerLayout;
     NavigationView navigationView;
@@ -35,6 +47,11 @@ public class ListNoteActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_list_note);
+
+        //truyen id
+        txtNameUser = (TextView) findViewById(R.id.txtNameUser);
+        Id = getIntent().getIntExtra("Id", 2);
+        txtNameUser.setText("Hi, "+ getName() + " !");
 
         // map
         toolbar = (Toolbar) findViewById(R.id.toolbar);
@@ -66,6 +83,33 @@ public class ListNoteActivity extends AppCompatActivity {
         menuAdapter = new MenuAdapter(this, R.layout.item_row_menu, arrList);
         listView.setAdapter(menuAdapter);
 
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                if(position == 0) {
+                    Intent intent = new Intent(ListNoteActivity.this, HomeActivity.class).putExtra("Id", Id);
+                    startActivity(intent);
+                }
+                if(position == 1) {
+                    Intent intent = new Intent(ListNoteActivity.this, CategoryActivity.class).putExtra("Id", Id);
+                    startActivity(intent);
+                }
+                if(position == 2) {
+                    Intent intent = new Intent(ListNoteActivity.this, ListPriorityActivity.class).putExtra("Id", Id);
+                    startActivity(intent);
+                }
+                if(position == 3) {
+                    Intent intent = new Intent(ListNoteActivity.this, ListStatusActivity.class).putExtra("Id", Id);
+                    startActivity(intent);
+                }
+                if(position == 4) {
+                    Intent intent = new Intent(ListNoteActivity.this, ListNoteActivity.class).putExtra("Id", Id);
+                    startActivity(intent);
+                }
+            }
+        });
+
+
         // action menu account
         arrListAccount = new ArrayList<ItemMenu>();
         arrListAccount.add(new ItemMenu("Edit Profile", R.drawable.ic_action_edit));
@@ -73,6 +117,19 @@ public class ListNoteActivity extends AppCompatActivity {
 
         menuAdapter = new MenuAdapter(this, R.layout.item_row_menu, arrListAccount);
         listViewAccount.setAdapter(menuAdapter);
+        listViewAccount.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                if(position == 0) {
+                    Intent intent = new Intent(ListNoteActivity.this, ChangeProfileActivity.class).putExtra("Id", Id);
+                    startActivity(intent);
+                }
+                if(position == 1) {
+                    Intent intent = new Intent(ListNoteActivity.this, ChangePasswordActivity.class).putExtra("Id", Id);
+                    startActivity(intent);
+                }
+            }
+        });
 
 
         // list note
@@ -97,5 +154,15 @@ public class ListNoteActivity extends AppCompatActivity {
         noteAdapter =new NoteAdapter(getApplicationContext(), arrNote);
         noteListRecycler.setAdapter(noteAdapter);
 
+    }
+    //Get name
+    private String getName(){
+        database = openOrCreateDatabase(DATABASE_NAME,MODE_PRIVATE,null);
+        String query = "select * from USER where Id = " + Id ;
+        Cursor cursor   = database.rawQuery(query,null);
+        cursor.moveToFirst();
+        String string = cursor.getString(1) ;
+        cursor.close();
+        return string;
     }
 }
